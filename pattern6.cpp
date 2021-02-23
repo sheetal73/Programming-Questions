5793ddcc9d0b6a307ee476a6f4c355ef32171a81
sheetal73 <49942844+sheetal73@users.noreply.github.com>
2021-02-23 15:04:29 +0530

    pattern 3 with numbers

---
+++ pattern6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n=5;
+	int i,j,k;
+	for(i=0;i<n;i++){
+		for( k=0;k<n-i-1;k++)
+			cout<<" ";
+		for( j=i+1;j>=1;j--){
+			cout<<j;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
