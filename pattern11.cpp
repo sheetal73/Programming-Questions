0a06fe1c81afcd72b563ce4e600672d1aff1e076
sheetal73 <49942844+sheetal73@users.noreply.github.com>
2021-02-23 15:59:14 +0530

    pattern 6 with * and space b/w them

---
+++ pattern11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n=5;
+	int i,j,k;
+	for(i=0;i<n;i++){
+		
+		for( j=0;j<i;j++){
+			cout<<" ";
+		}
+		for(k=0;k<n-i;k++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
